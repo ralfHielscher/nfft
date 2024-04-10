e202f0cba948321acd7fef8554e3751511e37370
theresa <theresa.wagner@mathematik.tu-chemnitz.de>
2024-04-10 15:34:56 +0200

    added kernel der_laplacian_rbf

--- applications/fastsum/kernels.c
+++ applications/fastsum/kernels.c
@@ -431,6 +431,23 @@ C laplacian_rbf(R x, int der, const R *param)    /* K(x)=EXP(-|x|/c) */
   return value;
 }
 
+C der_laplacian_rbf(R x, int der, const R *param)    /* K(x)=|x|/c EXP(-|x|/c) */
+{
+  R c = param[0];
+  R value = K(0.0);
+
+  switch (der)
+  {
+    case  0 : value = (FABS(x)/c)*EXP(-FABS(x)/c); break;
+    default:
+        value = (POW(K(-1.0),(R)der))*((FABS(x)-(R)der*c)/POW(c,(R)der+1))*EXP(-FABS(x)/c);
+        if (x < K(0.0))
+            value *= POW(K(-1.0),(R)der);
+  }
+
+  return value;
+}
+
 C xx_gaussian(R x, int der, const R *param)    /* K(x)=x^2/c^2 EXP(-x^2/c^2) */
 {
   R c = param[0];
